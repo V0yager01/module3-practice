3fc27e8500268305ba93e51efa46c5b443ff0722
Ilyas Khaliullin <ilyaskhaliullin05@gmail.com>
2025-08-15 19:36:03 +0500

    3

--- videoapp/api/admin.py
+++ videoapp/api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Video, VideoFile 
+from .models import Video, VideoFile, Like
 
 # Register your models here.
 admin.site.register(Video)
 admin.site.register(VideoFile)
+admin.site.register(Like)
